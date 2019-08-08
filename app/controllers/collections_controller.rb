952f2f455ff96c200f10097301b5e6d673da81b4
aaron-collier <aaron.collier@stanford.edu>
2019-08-08 15:24:00 -0700

    Various UI movement

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -5,4 +5,8 @@ class CollectionsController < ApplicationController
   def index
     @collections = Collection.order('title')
   end
+
+  def show
+    @collection = Collection.find(params[:id])
+  end
 end
