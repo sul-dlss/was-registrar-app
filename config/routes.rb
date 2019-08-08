9e74fda6c1824a60a75bb613857bd1d551f95e04
aaron-collier <aaron.collier@stanford.edu>
2019-08-08 14:40:00 -0700

    Simplify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'collections/show'
-  get 'collections/new'
-  get 'collections/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: 'collections#index'
   get '/', to: redirect('/collections')
 
-  resources :collections, only: [:index]
+  resources :collections
 end
