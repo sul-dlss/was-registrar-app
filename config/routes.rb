a729f27c22e538f247fdd9f77fbba07718b6e5de
Justin Littman <justinlittman@stanford.edu>
2019-08-08 13:04:42 -0400

    Adds list page.
    closes #18

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root to: 'collections#index'
+  get '/', to: redirect('/collections')
+
+  resources :collections, only: [:index]
 end
