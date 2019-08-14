3ad782b0c9c1bc9beacfe222f0a8a612ed5debb3
aaron-collier <aaron.collier@stanford.edu>
2019-08-14 11:36:34 -0700

    enumrate the routes we are using

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # root to: 'collections#index'
   get '/', to: redirect('/collections')
 
-  resources :collections
+  resources :collections, only: %i[edit index new show update]
 end
