89f170d068de5f8250e0bbf9ecee450687a4a84d
Justin Coyne <jcoyne@justincoyne.com>
2019-08-16 10:59:01 -0500

    Add active checkbox

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -48,6 +48,6 @@ class CollectionsController < ApplicationController
   private
 
   def collection_params
-    params.require(:collection).permit(:title, :druid, :embargo_months, :last_successful_fetch)
+    params.require(:collection).permit(:title, :druid, :embargo_months, :last_successful_fetch, :active)
   end
 end
--- spec/features/create_collection_spec.rb
+++ spec/features/create_collection_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe 'Create a collection', type: :feature do
       expect(page).to have_content "Druid can't be blank and Druid must begin with druid:"
     end
   end
+
+  context 'when all fields are present' do
+    it 'shows the errors' do
+      visit '/collections'
+      click_link 'Add a Collection'
+
+      fill_in 'Title', with: 'Robots'
+      fill_in 'Druid', with: "druid:#{rand(10_000)}"
+      check 'collection_active'
+      click_button 'Create Collection'
+
+      expect(page).to have_content 'Collection created.'
+    end
+  end
 end
