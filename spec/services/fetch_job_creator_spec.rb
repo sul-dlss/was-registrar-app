5930ad5b7688ba9dd65d49203f15c503fb9bb0d0
Justin Coyne <jcoyne@justincoyne.com>
2019-08-22 14:32:17 -0700

    Create FetchJobCreator
    closes #93

---
+++ spec/services/fetch_job_creator_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe FetchJobCreator do
+  before do
+    FetchMonth.destroy_all
+    allow(Date).to receive(:today).and_return(Date.parse('2019-08-21'))
+    allow(FetchJob).to receive(:perform_later)
+  end
+
+  context 'when there is an embargo' do
+    let(:collection) do
+      create(:collection, fetch_start_month: '2018-10-01')
+    end
+
+    context 'for a collection that has fetch months' do
+      before do
+        collection.fetch_months.create!(year: 2018, month: 11, status: 'waiting')
+      end
+
+      it 'creates fetch_months' do
+        described_class.run(druid: collection.druid)
+        expect(collection.fetch_months.map { |m| [m.year, m.month] }).to eq [
+          [2018, 11], [2018, 12], [2019, 1]
+        ]
+        # 2018-11 already exists, so won't be performed here.
+        expect(FetchJob).to have_received(:perform_later).exactly(2).times
+      end
+    end
+
+    context 'for a collection that does not have fetch months' do
+      it 'creates fetch_months' do
+        described_class.run(druid: collection.druid)
+        expect(collection.fetch_months.map { |m| [m.year, m.month] }).to eq [
+          [2018, 10], [2018, 11], [2018, 12], [2019, 1]
+        ]
+        expect(FetchJob).to have_received(:perform_later).exactly(4).times
+      end
+    end
+  end
+
+  context 'when there is no embargo' do
+    let(:collection) do
+      create(:collection, embargo_months: 0, fetch_start_month: '2018-10-01')
+    end
+
+    context 'for a collection that has fetch months' do
+      before do
+        collection.fetch_months.create!(year: 2018, month: 11, status: 'waiting')
+      end
+
+      it 'creates fetch_months' do
+        described_class.run(druid: collection.druid)
+        expect(collection.fetch_months.map { |m| [m.year, m.month] }).to eq [
+          [2018, 11], [2018, 12], [2019, 1], [2019, 2], [2019, 3], [2019, 4], [2019, 5], [2019, 6], [2019, 7]
+        ]
+        # 2018-11 already exists, so won't be performed here.
+        expect(FetchJob).to have_received(:perform_later).exactly(8).times
+      end
+    end
+
+    context 'for a collection that does not have fetch months' do
+      it 'creates fetch_months' do
+        described_class.run(druid: collection.druid)
+        expect(collection.fetch_months.map { |m| [m.year, m.month] }).to eq [
+          [2018, 10], [2018, 11], [2018, 12], [2019, 1], [2019, 2], [2019, 3], [2019, 4], [2019, 5], [2019, 6],
+          [2019, 7]
+        ]
+        expect(FetchJob).to have_received(:perform_later).exactly(10).times
+      end
+    end
+  end
+end
