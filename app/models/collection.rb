34d9673b7767d24ee41974d23b6ed7869f0b94b0
Justin Coyne <jcoyne@justincoyne.com>
2019-08-22 13:40:54 -0700

    Display the FetchMonths on the Collection edit page

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,6 +3,7 @@
 # Web archive collection
 class Collection < ApplicationRecord
   self.primary_key = 'druid'
+  has_many :fetch_months
   validates_presence_of :title, :druid, :embargo_months, :fetch_start_month, :wasapi_provider_account
   validates :active, inclusion: { in: [true, false] }
   validates :druid, format: { with: /druid:.+/,
