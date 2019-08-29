223a208addafe3559709ded097739fa5a3080ea9
Jeremy Nelson <jermnelson@gmail.com>
2019-08-29 12:18:57 -0600

    Changes display for crawl_item_druid as part of Month's status field. Added test.
    For #https://github.com/sul-dlss/was-registrar-app/issues/120

--- spec/models/fetch_month_spec.rb
+++ spec/models/fetch_month_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe FetchMonth, type: :model do
   let(:collection) { create(:collection) }
-  let(:fetch_month) { create(:fetch_month, collection: collection) }
+  let(:fetch_month) { create(:fetch_month, collection: collection, crawl_item_druid: 'druid:abc123') }
 
   describe '#crawl_directory' do
     it 'returns the correct crawl directory' do
@@ -22,6 +22,12 @@ RSpec.describe FetchMonth, type: :model do
       it 'returns the correct job directory with month padded' do
         expect(fetch_month.job_directory).to eq('AIT_915/2017_01')
       end
+    end
+  end
+
+  describe 'crawl_item_druid' do
+    it 'returns the fetch crawl item druid' do
+      expect(fetch_month.crawl_item_druid).to eq('druid:abc123')
     end
   end
 
