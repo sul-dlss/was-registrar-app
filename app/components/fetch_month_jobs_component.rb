69812b4366c174e6a61146c384a234a2e3e892b7
Justin Littman <justinlittman@stanford.edu>
2019-09-04 16:01:26 -0400

    List currently running jobs.
    closes #132

---
+++ app/components/fetch_month_jobs_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Draws a table of listing the FetchMonths that have current jobs
+class FetchMonthJobsComponent < ActionView::Component::Base
+  def initialize(fetch_months:)
+    @fetch_months = fetch_months
+  end
+
+  private
+
+  attr_reader :fetch_months
+end
