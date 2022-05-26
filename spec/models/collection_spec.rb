1e10ad8c55006530c6b2319cec92ed028fbb77bf
Justin Littman <justinlittman@stanford.edu>
2022-05-26 16:06:38 -0400

    Enabled new rubocops.

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -13,40 +13,40 @@ RSpec.describe Collection, type: :model do
 
   context 'with a valid collection' do
     it 'validates' do
-      expect(collection.valid?).to eq(true)
+      expect(collection.valid?).to be(true)
     end
   end
 
   context 'with an invalid collection' do
     it 'validates title' do
       collection.title = nil
-      expect(collection.valid?).to eq(false)
+      expect(collection.valid?).to be(false)
     end
 
     it 'validates druid' do
       collection.druid = 'abc123'
-      expect(collection.valid?).to eq(false)
+      expect(collection.valid?).to be(false)
     end
 
     it 'validates active' do
       collection.active = nil
-      expect(collection.valid?).to eq(false)
+      expect(collection.valid?).to be(false)
     end
 
     it 'validates embargo_months' do
       collection.embargo_months = nil
-      expect(collection.valid?).to eq(false)
+      expect(collection.valid?).to be(false)
     end
 
     it 'validates wasapi_provider_account' do
       collection.wasapi_provider = nil
       collection.wasapi_account = nil
-      expect(collection.valid?).to eq(false)
+      expect(collection.valid?).to be(false)
     end
 
     it 'validates wasapi_collection_id' do
       collection.wasapi_collection_id = nil
-      expect(collection.valid?).to eq(false)
+      expect(collection.valid?).to be(false)
     end
 
     it 'validates default admin_policy' do
@@ -55,7 +55,7 @@ RSpec.describe Collection, type: :model do
 
     it 'validates unique' do
       collection.wasapi_collection_id = '916'
-      expect(collection.valid?).to eq(false)
+      expect(collection.valid?).to be(false)
     end
   end
 end
