3e8f55d2a97f870fd58830507afb56d632b95d87
Justin Littman <justinlittman@stanford.edu>
2022-06-23 09:31:41 -0400

    Correctly use sequence in registration job factory.
    refs #468

--- spec/factories/registration_jobs.rb
+++ spec/factories/registration_jobs.rb
@@ -2,12 +2,12 @@
 
 FactoryBot.define do
   factory :registration_job do
-    sequence :job_directory do |_n|
-      'crawl{n}'
+    sequence :job_directory do |n|
+      "crawl#{n}"
     end
     collection { 'druid:bf172jb9970' }
-    sequence :source_id do |_n|
-      'sul:crawl{n}'
+    sequence :source_id do |n|
+      "sul:crawl#{n}"
     end
   end
 end
