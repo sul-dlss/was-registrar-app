bc236354b91e42e77705cb98f0ec45ab6d218d64
Laura Wrubel <lwrubel@stanford.edu>
2024-10-29 16:31:11 -0400

    Further 7.2 config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,8 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
-  # config.active_job.queue_name_prefix = "was_registrar_app_production"
+  config.active_job.queue_adapter = :sidekiq
+  config.active_job.queue_name_prefix = 'was_registrar_app_production'
 
   # Disable caching for Action Mailer templates even if Action Controller
   # caching is enabled.
